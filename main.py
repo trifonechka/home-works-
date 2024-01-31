7dd1afef386a67aceba59af10f1661d18811b1ef
trifonechka <trifonkayulia@yandex.ru>
2024-01-31 11:22:58 +0500

    hw

---
+++ main.py
@@ -0,0 +1,11 @@
+def print_params(morning_hours, evening_hours):
+    print('чето я вообще не пониаю ничего. Но ведь я занимаюсь утром и вечером по',
+          morning_hours, 'и', evening_hours, 'часа')
+
+
+my_list = [3, 2]
+
+for hour in my_list:
+    print('Ну вот(((')
+    print_params(morning_hours=hour, evening_hours=4)
+    print('умаялась')
